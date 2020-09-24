ad8932f82ac04a68503afe522c6d0e00589f27de
zhx446 <2553268591@qq.com>
2020-09-24 11:30:57 +0800

    9-24 11.30

--- milkteazhou/src/views/Shoppcar.vue
+++ milkteazhou/src/views/Shoppcar.vue
@@ -7,23 +7,21 @@
 					<img src="../assets/milktea.png" width="58px" height="23.5px" />
 				</template>
 				<template #right>
-					<img src="../assets/icon_more.png" width="19.5px" height="18px" class="nav-icon-more"/>
+					<img src="../assets/icon_more.png" width="19.5px" height="18px" class="nav-icon-more" />
 				</template>
 			</van-nav-bar>
 		</div>
-		<div class="shoppcar-banner">
-			<img src="../assets/shoppcar_banner.png" width="355px" height="100.5px"/>
-		</div>
+		<img src="../assets/shoppcar_banner.png" width="355px" height="100.5px" class="shoppcar-banner" />
 		<!-- 订单列表 -->
 		<div class="shoppcar-order">
 			<div class="order-top">
 				<div class="order-title">饮品订单</div>
-				<img src="../assets/icon_delete.png" width="12.5px" height="13.5px"/>
+				<img src="../assets/icon_delete.png" width="12.5px" height="13.5px" />
 			</div>
 			<div class="order-content">
 				<div class="order-left">
 					<div class="order-show">
-						<img src="../assets/order_circle.png" width="14.5px" height="14.5px"/>
+						<img src="../assets/order_circle.png" width="14.5px" height="14.5px" />
 						<img src="../assets/order-milktea.png" />
 						<div class="order-show-text">
 							<div>醉心黑摩卡</div>
@@ -32,7 +30,7 @@
 						</div>
 					</div>
 					<div class="order-choice">
-						<van-stepper v-model="value" theme="round" button-size="22" disable-input />
+						<van-stepper v-model="value" theme="round" button-size="11.5" disable-input />
 					</div>
 				</div>
 			</div>
