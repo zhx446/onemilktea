0d2a1384573188092d4e4a150386e9021dd232b1
zhx446 <58761665+zhx446@users.noreply.github.com>
2020-09-28 11:38:42 +0800

    9-28 11.38

--- milkteazhou/src/views/Payment.vue
+++ milkteazhou/src/views/Payment.vue
@@ -4,7 +4,7 @@
 		<div class="payment-nav">
 			<van-nav-bar title="确认订单" left-arrow @click-left="toBack" class="payment-nav-bg">
 				<template #left>
-					<van-icon name="arrow-left" color="#423026" size="18px"/>
+					<van-icon name="arrow-left" color="#423026" size="18px" />
 				</template>
 			</van-nav-bar>
 		</div>
@@ -19,6 +19,71 @@
 					</div>
 				</div>
 				<div class="address-right">自提</div>
+			</div>
+			<!-- 订单列表 -->
+			<div class="payment-order">
+				<div class="payment-order-list" v-for="item in paymentList" :key="item.id">
+					<div class="order-list-left">
+						<img :src="item.milkImg" width="70px" height="67px" />
+						<div class="left-title">
+							<div>{{item.milkTitle}}</div>
+							<div>{{item.milkText}}</div>
+						</div>
+					</div>
+					<div class="order-list-num">x{{item.num}}</div>
+					<div class="order-list-right">
+						<div>￥{{item.milkPrice}}</div>
+						<div>{{item.milkTip}}</div>
+					</div>
+				</div>
+			</div>
+
+			<!-- 订单列表2 -->
+			<div class="payment-order">
+				<div class="payment-order-list" v-for="item in paymentList2" :key="item.id">
+					<div class="order-list-left">
+						<img :src="item.milkImg2" width="70px" height="67px" />
+						<div class="left-title">
+							<div>{{item.milkTitle2}}</div>
+							<div>{{item.milkText2}}</div>
+						</div>
+					</div>
+					<div class="order-list-num">x{{item.num2}}</div>
+					<div class="order-list-right">
+						<div>￥{{item.milkPrice2}}</div>
+						<div>{{item.milkTip2}}</div>
+					</div>
+				</div>
+			</div>
+
+			<!-- 支付栏 -->
+			<div class="pay-content">
+				<van-cell-group class="pay-cell">
+					<van-cell class="cell-border">
+						<template #title>
+							<div class="wallet">壹杯钱包</div>
+						</template>
+						<template #default>
+							<div class="wallet-right">无可用</div>
+						</template>
+					</van-cell>
+					<van-cell class="cell-bg">
+						<template #title>
+							<div class="wallet">优惠劵</div>
+						</template>
+						<template #default>
+							<div class="stock-bg">20元饮品抵用金</div>
+						</template>
+					</van-cell>
+					<van-cell class="cell-mode" is-link>
+						<template #title>
+							<div class="wallet">支付方式</div>
+						</template>
+						<template #default>
+							<div class="mode-right">微信支付</div>
+						</template>
+					</van-cell>
+				</van-cell-group>
 			</div>
 		</div>
 	</div>
